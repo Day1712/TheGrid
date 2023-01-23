fcdea7d435bbb69ba5e3ef3ed8464dc5d54d7bf0
Rhodé <rhode@sp-byods-145-109-47-101.wireless.uva.nl>
2023-01-23 13:37:27 +0100

    added pseudo code from lecture

--- code/algorithms/better_algorithm.py
+++ code/algorithms/better_algorithm.py
@@ -1,2 +1,28 @@
 # TODO write algorithm that creates better routes than the
 # 'valid_shortest_route'. More overlap to lower the shared cost.
+
+'''
+Maybe Hillclimber?
+Pseudo code from the lecture:
+
+Kies een random start state
+Herhaal tot na N-keer niet meer verbetert:
+    Doe een kleine random aanpassing
+    Als de state is verslechterd:
+        Maak de aanpassing ongedaan
+
+
+
+Simulated Annealing,
+pseudo code of the lecture example:
+
+Herhaal:
+    Kies een random start state
+    Kies start temperatuur
+    Herhaal N iteraties:
+        Doe een kleine random aanpassing
+        Als random( ) > kans(oud, nieuw, temperatuur):
+            Maak de aanpassing ongedaan
+        Verlaag temperatuur
+
+'''
