fd51de788c92b24f6283210335c2ea3e41e250d7
Max Schuit <maximus.schuit@gmail.com>
2023-02-01 11:35:41 +0100

    Cleaned up.

--- code/visualisation/visualisation.py
+++ code/visualisation/visualisation.py
@@ -3,7 +3,7 @@ from code.classes import district
 import pylab as pl
 from matplotlib import collections
 
-def visualise(district):
+def basic_static_visualise(district):
     # Lists for the plot
     list_x = []
     list_y = []
@@ -103,67 +103,10 @@ def draw_district(district, fig, ax1, ax2, cost_list, iterations):
 
 def setup_plot(district):
     fig, (ax1, ax2) = plt.subplots(1, 2)
-    """# Lists for the plot of houses
-    house_x = []
-    house_y = []
-    house_colour = []
-    battery_x = []
-    battery_y = []
-    battery_colour = []
-
-    # Add the location and colour of each input to the lists
-    for house in district.connections:
-        house_x.append(house.pos_x)
-        house_y.append(house.pos_y)
-        house_colour.append(house.colour)
-        battery_x.append(district.connections[house].pos_x)
-        battery_y.append(district.connections[house].pos_y)
-        battery_colour.append(district.connections[house].colour)
-
-
-    return house_x, house_y, house_colour, battery_x, battery_y, battery_colour
-    """
     return fig, ax1, ax2
 
-def colour_visualise(district):
-    house_x = []
-    house_y = []
-    house_colour = []
-    battery_x = []
-    battery_y = []
-    battery_colour = []
 
-    # Add the location and colour of each input to the lists
-    for house in district.houses:
-        house_x.append(house.pos_x)
-        house_y.append(house.pos_y)
-        house_colour.append(house.colour)
-
-    for battery in district.batteries:
-        battery_x.append(battery.pos_x)
-        battery_y.append(battery.pos_y)
-        battery_colour.append(battery.colour)
-
-    # Change the figure size
-    plt.figure(figsize=(9,9))
-
-    # Make scatter plot with grid
-    plt.grid(which = 'both')
-    plt.scatter(house_x, house_y, c = house_colour, marker = 'o', zorder = 3)
-    plt.scatter(battery_x, battery_y, c = battery_colour, marker = 's', zorder = 3)
-    plt.xticks(range(min(house_x + battery_x), max(house_x + battery_x) + 1), fontsize = 7)
-    plt.yticks(range(min(house_y + battery_y), max(house_y + battery_y) + 1), fontsize = 7)
-
-    # Add the plot of the cables
-    for house in district.connections:
-        for segment in house.cables.segments:
-            x = [point[0] for point in segment]
-            y = [point[1] for point in segment]
-            plt.plot(x, y, c = house.colour)
-
-    plt.show()
-
-def updated_colour_visualise(district):
+def static_colour_visualise(district):
     house_x = []
     house_y = []
     house_colour = []
