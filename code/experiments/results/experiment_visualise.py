389db89041734ae169763656cc0df9cdfc3d3f4a
Max Schuit <maximus.schuit@gmail.com>
2023-02-02 17:36:44 +0100

    Added code for sim ann visualisation.

--- code/experiments/results/experiment_visualise.py
+++ code/experiments/results/experiment_visualise.py
@@ -22,7 +22,17 @@ hill_df = hill_df.astype({"district": "int64", "parameter": "int64", "shared": "
 
 print(hill_df)
 
+sim_df = pd.read_csv("simulated_annealing/experiment_sim_ann.csv", header=0, names=["iteration", "algorithm", "district", "parameter", "shared", "own", "time"])
 
+new = sim_df["parameter"].str.split(" and", n = 1, expand = True)
+new1 = new[0].str.split("of ", n = 1, expand = True)
+new2 = new[1].str.split("of ", n = 1, expand = True)
+
+sim_df.insert(4, "temp", new1[1])
+sim_df.insert(5, "random_selection", new2[1])
+sim_df = sim_df.drop(columns=["iteration", "algorithm", "parameter"])
+sim_df = sim_df.astype({"district": "int64", "temp": "int64", "random_selection": "int64", "shared": "int64", "own": "int64"})
+print(sim_df)
 
 
 
